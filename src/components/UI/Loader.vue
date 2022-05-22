b5e95e1fada3a93979e3a184e1c84371e28a8103
Patrick Porto <patrickporto@MacBook-Air-de-Patrick.local>
2022-05-22 16:42:30 -0300

    :sparkles: adding loader component

---
+++ src/components/UI/Loader.vue
@@ -0,0 +1,23 @@
+<template>
+  <div :class="classes"></div>
+</template>
+
+<script>
+export default {
+  name: 'appmax-loader',
+
+  props: {
+    size: {
+      type: String,
+      required: false,
+      default: 'medium',
+    },
+  },
+
+  computed: {
+    classes() {
+      return `loading loading--${this.size}`;
+    },
+  },
+};
+</script>
--- src/components/home/CardResult.vue
+++ src/components/home/CardResult.vue
@@ -1,36 +1,46 @@
 <template>
-  <appmax-rotate-card v-if="typeName" :cardType="typeName">
-    <div slot="frontside" class="card-result">
-      <picture class="card-result__picture">
-        <img
-          :src="pokemon && pokemon.sprites && pokemon.sprites.front_default"
-          alt=""
-        />
-      </picture>
+  <div>
+    <appmax-loader v-if="loading" />
 
-      <p class="card-result__id">#{{ pokemon.id }}</p>
-      <p class="card-result__title">{{ pokemon.name }}</p>
-      <p class="card-result__type">Tipo: {{ typeName }}</p>
-    </div>
+    <appmax-rotate-card v-else :cardType="typeName">
+      <div slot="frontside" class="card-result">
+        <picture class="card-result__picture">
+          <img
+            :src="pokemon && pokemon.sprites && pokemon.sprites.front_default"
+            alt=""
+          />
+        </picture>
 
-    <div slot="backside" class="card-result">
-      <h2 class="card-result__detail card-result__detail--title">Detalhes</h2>
-      <p class="card-result__detail">Id: {{ pokemon.id }}</p>
-      <p class="card-result__detail">Nome: {{ pokemon.name }}</p>
-      <p class="card-result__detail">Tipo: {{ typeName }}</p>
-      <p class="card-result__detail">Peso: {{ pokemon.weight }} g</p>
-      <p class="card-result__detail">Altura: {{ pokemon.height }} cm</p>
-      <p class="card-result__detail">XP base: {{ pokemon.base_experience }}</p>
-    </div>
-  </appmax-rotate-card>
+        <p class="card-result__id">#{{ pokemon.id }}</p>
+        <p class="card-result__title">{{ pokemon.name }}</p>
+        <p class="card-result__type">Tipo: {{ typeName }}</p>
+      </div>
+
+      <div slot="backside" class="card-result">
+        <h2 class="card-result__detail card-result__detail--title">Detalhes</h2>
+        <p class="card-result__detail">Id: {{ pokemon.id }}</p>
+        <p class="card-result__detail">Nome: {{ pokemon.name }}</p>
+        <p class="card-result__detail">Tipo: {{ typeName }}</p>
+        <p class="card-result__detail">Peso: {{ pokemon.weight }} g</p>
+        <p class="card-result__detail">Altura: {{ pokemon.height }} cm</p>
+        <p class="card-result__detail">
+          XP base: {{ pokemon.base_experience }}
+        </p>
+      </div>
+    </appmax-rotate-card>
+  </div>
 </template>
 
 <script>
 import pokemonApi from "@/api/requests/pokemon";
 import AppmaxRotateCard from "@/components/UI/RotateCard.vue";
+import AppmaxLoader from "@/components/UI/Loader.vue";
+import { loading } from "@/mixins/loading";
 
 export default {
-  components: { AppmaxRotateCard },
+  components: { AppmaxRotateCard, AppmaxLoader },
+
+  mixins: [loading],
 
   props: {
     selected: {
@@ -64,14 +74,14 @@ export default {
 
   methods: {
     async getSelected(selected) {
-      this.$emit("startLoading");
+      this.showLoader(true);
       try {
         const { data } = await pokemonApi.getByName(selected);
         this.pokemon = data;
       } catch (error) {
         this.$emit("error", error);
       } finally {
-        this.$emit("finishLoading");
+        this.showLoader(false);
       }
     },
   },
